76b77a4d1ca6c5c94f33516cc36a94436e479eb3
aryanshettar007 <aryanshettar007@gmail.com>
2026-01-21 16:27:39 +0530

    Update gemini model

--- server/controllers/messageController.js
+++ server/controllers/messageController.js
@@ -16,7 +16,7 @@ export const textMessageController = async (req,res)=>{
         chat.messages.push({role:"user",content:prompt,isImage:false,timestamp:Date.now()});
 
         const {choices} = await openai.chat.completions.create({
-    model: "gemini-2.0-flash",
+    model: "gemini-3-flash-preview",
     messages: [
         {
             role: "user",
